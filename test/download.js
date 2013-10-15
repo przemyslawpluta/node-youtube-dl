429687cd34c160be60196e05b0b39e4e9d1ec2da
Roly Fentanes <roly426@gmail.com>
2013-10-15 01:00:23 -0400

    specify format to download

--- test/download.js
+++ test/download.js
@@ -9,7 +9,7 @@ var video      = 'http://www.youtube.com/watch?v=90AiXO1pAiA';
 vows.describe('download').addBatch({
   'a video': {
     'topic': function() {
-        var dl = ytdl.download(video, __dirname),
+        var dl = ytdl.download(video, __dirname, ['-f', '18']),
             cb = this.callback;
 
         dl.on('error', cb);
@@ -35,7 +35,7 @@ vows.describe('download').addBatch({
       assert.isObject(data);
       assert.equal(data.id, '90AiXO1pAiA');
       assert.isTrue(/lol-90AiXO1pAiA/.test(data.filename));
-      assert.equal(data.size, '1.26MiB');
+      assert.equal(data.size, '918.57KiB');
       assert.isNumber(data.timeTakenms);
       assert.isString(data.timeTaken);
       assert.isNumber(data.averageSpeedBytes);
