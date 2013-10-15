c7de217324dea4754127ee9b6a399ba1c125fa57
Roly Fentanes <roly426@gmail.com>
2013-10-14 23:46:58 -0400

    always download latest version of youtube-dl

--- scripts/download.js
+++ scripts/download.js
@@ -1,6 +1,7 @@
-var fs   = require('fs');
-var path = require('path');
-var http = require('http');
+var fs    = require('fs');
+var path  = require('path');
+var http  = require('http');
+var https = require('https');
 
 var dir = path.join(__dirname, '..', 'bin');
 var filename = 'youtube-dl';
@@ -12,22 +13,57 @@ if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir, 484);
 }
 
-// Download youtube-dl.
-var url = 'http://youtube-dl.org/downloads/2013.07.12/youtube-dl';
-http.get(url, function(res) {
-  if (res.statusCode !== 200) {
-    throw Error('Response Error: ' + res.statusCode);
-  }
+// First, look for the download link.
+var regexp = /https:\/\/yt-dl\.org\/downloads\/\d{4}\.\d\d\.\d\d\/youtube-dl/;
+function getDownloadLink() {
+  var url = 'http://rg3.github.io/youtube-dl/download.html';
+  http.get(url, function(res) {
+    var body = '';
+    res.setEncoding('utf8');
 
-  res.pipe(fs.createWriteStream(filepath));
-  res.on('end', function() {
-    // Make file executable.
-    fs.chmodSync(filepath, 457);
-    console.log('Finished!');
+    res.on('data', function(data) {
+      body += data;
+    });
+
+    res.on('end', function() {
+      var m = regexp.exec(body);
+      if (m) {
+        download(m[0]);
+      } else {
+        console.error('Could not find download link in ' + url);
+      }
+    });
+
+    res.on('error', function(err) {
+      throw err;
+    });
+  }).on('error', function(err) {
+    throw err;
   });
+}
 
-}).on('error', function(err) {
-  throw err;
-});
+// Download youtube-dl.
+function download(link) {
+  https.get(link, function(res) {
+    if (res.statusCode !== 200) {
+      throw Error('Response Error: ' + res.statusCode);
+    }
+
+    res.pipe(fs.createWriteStream(filepath));
+    res.on('end', function() {
+      // Make file executable.
+      fs.chmodSync(filepath, 457);
+      console.log('Finished!');
+    });
+
+    res.on('error', function(err) {
+      throw err;
+    });
+
+  }).on('error', function(err) {
+    throw err;
+  });
+}
 
 console.log('Downloading latest youtube-dl');
+getDownloadLink();
