ac48c3745e9f55e950f846d93eda9d32d2e627e0
Roly Fentanes <roly426@gmail.com>
2014-06-14 16:32:13 -0400

    fix parsing resolutions

--- test/getFormat.js
+++ test/getFormat.js
@@ -7,6 +7,13 @@ var video  = 'http://www.youtube.com/watch?v=0k2Zzkw_-0I';
 var expected = [
   { id: '0k2Zzkw_-0I', itag: 171, filetype: 'webm', resolution: 'audio only' },
   { id: '0k2Zzkw_-0I', itag: 140, filetype: 'm4a', resolution: 'audio only' },
+  { id: '0k2Zzkw_-0I', itag: 160, filetype: 'mp4', resolution: '144p' },
+  { id: '0k2Zzkw_-0I', itag: 242, filetype: 'webm', resolution: '240p' },
+  { id: '0k2Zzkw_-0I', itag: 133, filetype: 'mp4', resolution: '240p' },
+  { id: '0k2Zzkw_-0I', itag: 243, filetype: 'webm', resolution: '360p' },
+  { id: '0k2Zzkw_-0I', itag: 134, filetype: 'mp4', resolution: '360p' },
+  { id: '0k2Zzkw_-0I', itag: 244, filetype: 'webm', resolution: '480p' },
+  { id: '0k2Zzkw_-0I', itag: 135, filetype: 'mp4', resolution: '480p' },
   { id: '0k2Zzkw_-0I', itag: 17, filetype: '3gp', resolution: '176x144' },
   { id: '0k2Zzkw_-0I', itag: 36, filetype: '3gp', resolution: '320x240' },
   { id: '0k2Zzkw_-0I', itag: 5, filetype: 'flv', resolution: '400x240' },
