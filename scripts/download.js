f49128c5e6f9622f7492b0e3e1d7306610251df0
przemyslawpluta <przemekpluta@hotmail.com>
2016-01-23 09:19:54 +0000

    callback err

--- scripts/download.js
+++ scripts/download.js
@@ -1,6 +1,7 @@
 var downloader = require('../lib/downloader');
 
-downloader(function error(err) {
+downloader(function error(err, done) {
   'use strict';
-  if (err) { return console.log(err); }
+  if (err) { return console.log(err.stack); }
+  console.log(done);
 });
