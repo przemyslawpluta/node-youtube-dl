d2a32694f9a8ff31cbd98fb53c0b47e63ca9a51d
przemyslawpluta <przemekpluta@hotmail.com>
2016-03-14 21:23:48 +0000

    fix resolution or audio

--- example/formats.js
+++ example/formats.js
@@ -4,10 +4,14 @@ var url = 'http://www.youtube.com/watch?v=0RUvealeXZ0';
 
 function mapInfo(item) {
   'use strict';
-  return {itag: item.format_id, filetype: item.ext, resolution: item.resolution || 'audio only'};
+  return {
+  	itag: item.format_id,
+  	filetype: item.ext,
+  	resolution: item.resolution || ((item.width) ? item.width + 'x' + item.height : 'audio only')
+  };
 }
 
-ytdl.getInfo(url, function getFormats(err, info) {
+ytdl.getInfo(url, function getInfo(err, info) {
   'use strict';
   if (err) { throw err; }
   var formats = {id: info.id, formats: info.formats.map(mapInfo)};
