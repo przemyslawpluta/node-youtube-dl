acb1571af84c14fe3c861641fcf5c26d1e9d950a
przemyslawpluta <przemekpluta@hotmail.com>
2016-03-18 21:24:41 +0000

    handle playlists

--- example/playlist.js
+++ example/playlist.js
@@ -2,30 +2,38 @@ var path = require('path');
 var fs   = require('fs');
 var ytdl = require('..');
 
-var video = ytdl('https://www.youtube.com/playlist?list=PLEFA9E9D96CB7F807');
+function playlist(url) {
 
-video.on('error', function error(err) {
   'use strict';
-  console.log('error 2:', err);
-});
+  var video = ytdl(url);
 
-var size = 0;
-video.on('info', function(info) {
-  'use strict';
-  size = info.size;
-  var output = path.join(__dirname + '/', size + '.mp4');
-  video.pipe(fs.createWriteStream(output));
-});
+  video.on('error', function error(err) {
+    console.log('error 2:', err);
+  });
 
-var pos = 0;
-video.on('data', function data(chunk) {
-  'use strict';
-  pos += chunk.length;
-  // `size` should not be 0 here.
-  if (size) {
-    var percent = (pos / size * 100).toFixed(2);
-    process.stdout.cursorTo(0);
-    process.stdout.clearLine(1);
-    process.stdout.write(percent + '%');
-  }
-});
+  var size = 0;
+  video.on('info', function(info) {
+    size = info.size;
+    var output = path.join(__dirname + '/', size + '.mp4');
+    video.pipe(fs.createWriteStream(output));
+  });
+
+  var pos = 0;
+  video.on('data', function data(chunk) {
+    pos += chunk.length;
+    // `size` should not be 0 here.
+    if (size) {
+      var percent = (pos / size * 100).toFixed(2);
+      process.stdout.cursorTo(0);
+      process.stdout.clearLine(1);
+      process.stdout.write(percent + '%');
+    }
+  });
+
+  video.on('next', function next(data) {
+    playlist(data);
+  });
+
+}
+
+playlist('https://www.youtube.com/playlist?list=PLEFA9E9D96CB7F807');
