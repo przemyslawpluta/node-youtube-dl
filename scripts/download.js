44adbc0a16241036b0d6de51e107f1d67c261077
Gabriel Freire <Gabriel Freire>
2017-07-23 20:29:42 +0100

    Refactor inline code and fixed tests

--- scripts/download.js
+++ scripts/download.js
@@ -1,7 +1,9 @@
 var downloader = require('../lib/downloader');
 
 downloader(function error(err, done) {
-  'use strict';
-  if (err) { return console.log(err.stack); }
-  console.log(done);
+    'use strict';
+    if (err) {
+        return console.log(err.stack);
+    }
+    console.log(done);
 });
