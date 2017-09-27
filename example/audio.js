17c3fb14dbe7df64a947899040230f08e1819e0c
przemyslawpluta <przemekpluta@hotmail.com>
2017-09-27 21:24:12 +0100

    update audio.js

--- example/audio.js
+++ example/audio.js
@@ -2,6 +2,8 @@ var ytdl = require('..');
 
 var url = 'https://www.youtube.com/watch?v=H7HmzwI67ec';
 
+// pass ['-f', 'bestaudio'] || ['-f', 'm4a'] for other formats with yt
+
 ytdl.exec(url, ['-x', '--audio-format', 'mp3'], {}, function exec(err, output) {
     'use strict';
     if (err) { throw err; }
